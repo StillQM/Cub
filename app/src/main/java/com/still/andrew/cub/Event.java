27f786cee58eec8281e787ae14d1c2868dba49d6
aes5638 <aes5638@psu.edu>
2016-02-10 12:27:59 -0500

    Rewrote EventActivity and added Event class

---
+++ app/src/main/java/com/still/andrew/cub/Event.java
@@ -0,0 +1,25 @@
+package com.still.andrew.cub;
+
+/**
+ * Created by aes5638 on 2/10/16.
+ */
+public class Event {
+
+    private String eventName;
+
+    public Event(){
+
+    }
+
+    public Event(String eventName){
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+
+
+
+}
--- app/src/main/java/com/still/andrew/cub/EventActivity.java
+++ app/src/main/java/com/still/andrew/cub/EventActivity.java
@@ -1,64 +1,58 @@
 package com.still.andrew.cub;
 
-import android.support.v7.app.AppCompatActivity;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class EventActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-    String firebaseURL;
-
+    private String firebaseURL;
+    Event event;
+    Intent intent = getIntent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event);
+        setContentView(R.layout.activity_main);
 
-        final TextView titleText = (TextView) findViewById(R.id.event_title);
-
-        String selectionID;
-        if (savedInstanceState == null) {
+        String newString;
+        if(savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if(extras == null) {
-                selectionID = null;
+                newString = null;
             } else {
-                selectionID = extras.getString("ITEM_ID");
+                newString = extras.getString(("ITEM_ID"));
             }
         } else {
-            selectionID = (String) savedInstanceState.getSerializable("ITEM_ID");
+            newString = (String) savedInstanceState.getSerializable("ITEM_ID");
         }
 
-        firebaseURL = ("https://glaring-heat-9011.firebaseio.com/eventItems/February2016/10/" + selectionID);
+        firebaseURL = ("https://glaring-heat-9011.firebaseio.com/eventItems/February2016/10/" + newString);
 
 
 
@@ -66,8 +60,9 @@ public class EventActivity extends AppCompatActivity {
                 .addChildEventListener(new ChildEventListener() {
                     @Override
                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                        titleText.setText((String) dataSnapshot.child("eventName").getValue().toString());
-                        System.out.println((String) dataSnapshot.child("eventName").getValue().toString());
+                        event = new Event((String)dataSnapshot.child("eventName").getValue());
+                        System.out.println(event.getEventName());
+
                     }
 
                     @Override
@@ -77,6 +72,7 @@ public class EventActivity extends AppCompatActivity {
 
                     @Override
                     public void onChildRemoved(DataSnapshot dataSnapshot) {
+
                     }
 
                     @Override
@@ -88,105 +84,12 @@ public class EventActivity extends AppCompatActivity {
                     public void onCancelled(FirebaseError firebaseError) {
 
                     }
-
-
                 });
 
 
+
+
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_building, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_event, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.event_title);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
